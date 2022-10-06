dba10ffa7ebe24effda1ebd2e0e63fe08097c7b0
Marwan Zayed <84870483+marwanfabrejr@users.noreply.github.com>
2022-10-06 11:17:12 +0200

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h2 className='section-title'>about us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas fugiat,
+        beatae debitis repudiandae facilis at, aspernatur vero reiciendis
+        temporibus mollitia error quaerat sunt illum rerum distinctio? Id,
+        impedit? Doloribus consectetur ea architecto commodi nulla ducimus
+        repellendus iste cupiditate quis a.
+      </p>
+    </section>
+  )
+}
+
+export default About
